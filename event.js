cb504d625b0cd78ffdf500f39545c88e15d852d4
tawsif297 <tawsif928@gmail.com>
2025-08-20 22:22:31 +0600

    Add files via upload

---
+++ event.js
@@ -0,0 +1,15 @@
+function typeAndScream(){
+    var personName=document.getElementById("input").value;
+    alert("itold you not to cilck this "+ personName)
+;
+}
+
+function goDark(){
+
+    document.body.className="dark"
+
+}
+function finishTheStory(){
+    document.getElementById("story").innerHTML=
+"So I carefully searched the damp dungeon. There you are!"
+}
